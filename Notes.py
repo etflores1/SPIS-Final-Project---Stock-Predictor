7219ff7c3255437e7ff4f311e0f31e48e8d864f0
Ethan Flores <110417575+EthanFlores1@users.noreply.github.com>
2022-08-29 16:59:01 +0000

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,14 @@
+'''
+GAMEPLAN
+
+1) Set Up Python Packages and learn how they work (libararies)
+2) Find what data we will use
+3) Set up regression equation
+    - maybe another equation?
+4) Implement Seaborn graphs?
+
+SCHEDULE
+Saturday: 9 - 11:30 am
+Sunday: 1 - 4 pm
+
+'''
---
+++ main.py
@@ -0,0 +1,82 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+'''
+Entering in the stock of choice and getting the information for that stock
+'''
+chosen_stock = yf.Ticker(input('Please enter a stock ticker: '))
+stock_info = chosen_stock.info
+# stock_info.keys() for other properties you can explore
+
+'''
+Getting certain information of market
+'''
+market_price = stock_info['regularMarketPrice']
+previous_close_price = stock_info['regularMarketPreviousClose']
+    # day before market_price
+print('market price = ', market_price)
+print('previous close price = ', previous_close_price)
+
+'''
+Getting history of the stock
+'''
+hist = chosen_stock.history(period="max")
+
+'''
+Add a column which uses a counter instead of a date
+'''
+
+# print(type(hist))
+print(hist.columns, '\n')
+hist = hist.drop(columns = ["High", "Low", "Stock Splits", "Dividends", "Volume"])
+
+Counter = []
+for c in range(1, len(hist.index)+1):
+    Counter.append(c)
+
+hist['Counter'] = Counter
+# hist["Counter"] = hist.index
+
+
+'''
+Define the training and testing data for the linear regression equation
+'''
+
+len_train = int(len(hist)*0.8)
+train = hist["Close"][:len_train]
+test = hist["Close"][len_train:])
+print('hist length = ', len(hist))
+print('train length = ', len(train))
+print('test length = ', len(test))
+
+'''
+Creating a graph and regression equation
+'''
+x = [i for i in range(len(test))]#hist.Counter
+y = test
+# #create basic scatterplot 
+plt.plot(x, y, 'o') 
+ 
+# #obtain m (slope) and b(intercept) of linear regression line 
+m, b = np.polyfit(x, y, 1) 
+ 
+# #add linear regression line to scatterplot  
+plt.plot(x, m*x+b)		
+
+
+# sns.relplot(
+#     data=hist,
+#     x="Date", y="Close"
+# )
+plt.show()
+
+'''
+Final commands
+'''
+# hist[close]
+#print(hist[["Counter", "Open", "Close"]])
+# print(hist.index)
+# print(hist)
+
