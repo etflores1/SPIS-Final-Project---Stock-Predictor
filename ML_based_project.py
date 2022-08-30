112127e6a2f494e6646021af7a39e21c04dfc621
gavividra <gvidra@ucsd.edu>
2022-08-30 16:43:36 -0400

    L

--- ML_based_project.py
+++ ML_based_project.py
@@ -8,35 +8,9 @@ The point of this project is to get a prediction of the market price over time o
 given the history of the stocks in the SMP 500
 '''
 
-STOCKS = ["TSLA", "MSFT", "AAPL", "AMZN", "GOOGL", "META", "NFLX", "NVDA"]
-for stock in STOCKS:
+stocks = ["TSLA", "MSFT", "AAPL", "AMZN", "GOOGL", "META", "NFLX", "NVDA"]
+for stock in stocks:
     chosen_stock = yf.Ticker(stock)
     hist = chosen_stock.history(period="max")
-
-    hist = hist.drop(columns = ["High", "Low", "Stock Splits", "Dividends", "Volume"])
-
-    Counter = []
-    for c in range(1, len(hist.index)+1):
-        Counter.append(c)
-
-    hist['Counter'] = Counter
-    # hist["Counter"] = hist.index
-
-
-    '''
-    Define the training and testing data for the linear regression equation
-    '''
-
-    len_train = int(len(hist)*0.8)
-    train = hist["Close"][:len_train]
-    test = hist["Close"][len_train:]
-    print('hist length = ', len(hist))
-    print('train length = ', len(train))
-    print('test length = ', len(test))
-
-    '''
-    Creating a graph and regression equation
-    '''
-    x = hist.Counter 
-    # x = [i for i in range(len(train))]
-    y = hist.Close 
+    hist = hist.drop(columns = ["High", "Low", "Stock Splits", "Dividends", "Volume", "Open"])
+    print(chosen_stock, len(hist))
